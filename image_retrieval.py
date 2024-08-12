6e3fbef5554525aa737df74b1678b5888feb2188
Truong Vo <85341526+Truongvo307@users.noreply.github.com>
2024-08-12 17:09:24 +0900

    Upload project image retrieval
    Upload project image retrieval

---
+++ image_retrieval.py
@@ -0,0 +1,114 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+ROOT = 'data'
+CLASS_NAME = sorted(list(os.listdir(f'{ROOT}/train')))
+
+
+def read_image_from_path(path, size):
+    im = Image.open(path).convert('RGB').resize(size)
+    return np.array(im)
+
+
+def folder_to_images(folder, size):
+    list_dir = [f'folder/{name}' for name in os.listdir(folder)]
+    images_np = np.zeros(shape=(len(list_dir), *size, 3))
+    images_path = []
+    for i, path in enumerate(list_dir):
+        images_np[i] = read_image_from_path(path, size)
+        images_path.append(path)
+    images_path = np.array(images_path)
+    return images_np, images_path
+
+
+def absolute_difference(query, data):
+    axis_batch_size = tuple(range(1, len(data.shape)))
+    return np.sum(np.abs(data-query), axis=axis_batch_size)
+
+
+def mean_square_difference(query, data):
+    axis_batch_size = tuple(range(1, len(data.shape)))
+    return np.mean((data - query)**2, axis=axis_batch_size)
+
+
+def cosine_similarity(query, data):
+    axis_batch_size = tuple(range(1, len(data.shape)))
+    query_norm = np.sqrt(np.sum(query**2))
+    data_norm = np.sqrt(np.sum(data**2, axis=axis_batch_size))
+    return np.sum(data * query, axis=axis_batch_size) / (query_norm*data_norm + np.finfo(float).eps)
+
+
+def correlation_coefficient(query, data):
+    axis_batch_size = tuple(range(1, len(data.shape)))
+    query_mean = query - np.mean(query)
+    data_mean = data - np.mean(data, axis=axis_batch_size, keepdims=True)
+    query_norm = np.sqrt(np.sum(query_mean**2))
+    data_norm = np.sqrt(np.sum(data_mean**2, axis=axis_batch_size))
+    return np.sum(data_mean * query_mean, axis=axis_batch_size) / (query_norm*data_norm + np.
+                                                                   finfo(float).eps)
+
+
+def get_l1_score(root_img_path, query_path, size):
+    query = read_image_from_path(query_path, size)
+    ls_path_score = []
+    for folder in os.listdir(root_img_path):
+        if folder in CLASS_NAME:
+            path = root_img_path + '/' + folder
+            image_np, image_path = folder_to_images(path, size)
+            rates = absolute_difference(query, image_np)
+            ls_path_score.append(list(zip(image_path, rates)))
+    return query, ls_path_score
+
+
+def get_l2_score(root_img_path, query_path, size):
+    query = read_image_from_path(query_path, size)
+    ls_path_score = []
+    for folder in os.listdir(root_img_path):
+        if folder in CLASS_NAME:
+            path = root_img_path + '/' + folder
+            image_np, image_path = folder_to_images(path, size)
+            rates = mean_square_difference(query, image_np)
+            ls_path_score.append(list(zip(image_path, rates)))
+    return query, ls_path_score
+
+
+def get_cosine_similarity_score(root_img_path, query_path, size):
+    query = read_image_from_path(query_path, size)
+    ls_path_score = []
+    for folder in os.listdir(root_img_path):
+        if folder in CLASS_NAME:
+            path = root_img_path + folder
+            images_np, images_path = folder_to_images(
+                path, size)  # mang numpy nhieu anh,
+            rates = cosine_similarity(query, images_np)
+            ls_path_score.extend(list(zip(images_path, rates)))
+    return query, ls_path_score
+
+
+def get_correlation_coefficient_score(root_img_path, query_path, size):
+    query = read_image_from_path(query_path, size)
+    ls_path_score = []
+    for folder in os.listdir(root_img_path):
+        if folder in CLASS_NAME:
+            path = root_img_path + folder
+            images_np, images_path = folder_to_images(
+                path, size)  # mang numpy nhieu anh,
+            rates = correlation_coefficient(query, images_np)
+            ls_path_score.extend(list(zip(images_path, rates)))
+    return query, ls_path_score
+
+
+if __name__ == '__main__':
+    root_img_path = f'{ROOT}/train/'
+    query_path = f"{ROOT}/test/Orange_easy/0_100.jpg"
+    # query_path = f"{ROOT}/test/African_crocodile/n01697457_18534.JPEG"
+    size = (448, 448)
+    # query, ls_path_score = get_l1_score(root_img_path, query_path, size)
+    # query, ls_path_score = get_l2_score(root_img_path, query_path, size)
+    query, ls_path_score = get_cosine_similarity_score(root_img_path, query_path, size)
+    plot_results(query_path, ls_path_score, reverse=False)
+    #correlation_coefficient
+    query, ls_path_score = get_correlation_coefficient_score(root_img_path, query_path, size)
+    plot_results(query_path, ls_path_score, reverse=True)
