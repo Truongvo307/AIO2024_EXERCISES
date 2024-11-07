14a0e1910ecffa8bf1180fc8429d2754ee54f096
Truong Vo <85341526+Truongvo307@users.noreply.github.com>
2024-11-07 15:37:36 +0900

    Update folder
    Update folder

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # calculate sum a,b
-def sum(a, b):
-    return a+b
-
 
 if __name__ == "__main__":
-    assert sum(2, 3) == 5
-    print(sum(2, 3))
-    print("Test pass")
+    print("This is the repo for AIO2024 _ Exercise")
+    print("I'm Truong")
